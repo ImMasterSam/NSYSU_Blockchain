a6429ca84f11d7439f5587e9f966b705d5fc5cc3
ImMasterSam <sam9630147@gmail.com>
2025-05-29 00:03:17 +0800

    Fix HW4 from HW3

--- HW 4/ecc/Script.py
+++ HW 4/ecc/Script.py
@@ -16,6 +16,19 @@ class Script:
         else:
             self.cmds = cmds
 
+    def __repr__(self) -> str:
+        result = []
+        for cmd in self.cmds:
+            if type(cmd) == int:
+                if OP_CODE_NAMES.get(cmd):
+                    name = OP_CODE_NAMES.get(cmd)
+                else:
+                    name = 'OP_[{}]'.format(cmd)
+                result.append(name)
+            else:
+                result.append(cmd.hex())
+        return ' '.join(result)
+
     @classmethod
     def parse(cls, s: BytesIO) -> Script:
         length = read_varint(s)
